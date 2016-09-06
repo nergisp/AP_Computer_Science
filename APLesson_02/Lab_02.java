5fb9f8183a71bc168a5939a47219a728df19eee5
nergisp <poseindonpiril@gmail.com>
2016-09-06 09:06:26 -0700

    second project
    multiplication, concentation, calculation

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,24 @@
+public class Lab_02 {
+
+	public static void main(String[]args) {
+	
+		// Multiplication
+		int x = 5;
+		int y = 4;
+		int z = x*y;
+		System.out.println(x + " multiplied by " + y + " is " + z + "\n");
+	
+		// Concatentation
+		System.out.println("Piril Nergis \n" + "23562 El Casa \n" + "Batman, Turkey \n");
+	
+		// Complex Calculation
+		int w = 2;
+		int l = 5;
+		int h = 8;
+		System.out.println("The dimesions of the cube are " + w + "," + l + "," + h);
+		int a = (2*w*l + 2*l+h + 2*w*h);
+		System.out.println("The surface area of the cube is " + a);
+		
+	}
+
+}
