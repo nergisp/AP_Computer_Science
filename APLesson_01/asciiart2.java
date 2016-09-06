8f753251ed18d31fb4925dc13f91958c4b681786
nergisp <poseindonpiril@gmail.com>
2016-09-06 08:25:10 -0700

    asciiartprojects
    one's a tetrahedron and the other is a bonsai tree

---
+++ APLesson_01/asciiart2.java
@@ -0,0 +1,15 @@
+public class asciiart2 {
+	
+	public static void main(String[]args) {
+		
+		System.out.println("         #o#");
+		System.out.println("       ####o#");
+		System.out.println("      #o# \\#|_#,#");
+		System.out.println("     ###\\ |/   #o#");
+		System.out.println("      # {}{      #");
+		System.out.println("ejm      }{{");
+		System.out.println("        ,'  `");
+		
+	}
+	
+}
