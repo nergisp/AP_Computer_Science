e22cbeb572b1dd18e0e259d0676219114d7914ca
nergisp <poseindonpiril@gmail.com>
2017-04-18 08:40:50 -0700

    Update Satellite1.java

--- APLesson_14/14.1/Satellite1.java
+++ APLesson_14/14.1/Satellite1.java
@@ -4,16 +4,15 @@ public class Satellite
    public static void main(String[]args)
    {
        ArrayList<Location> locate = new ArrayList<>();
-       double[] honLoc = {5, 6};
+	   
+	   double[] honLoc = {5, 6};
+	   double[] home = {0, 0};
+	   
        locate.add(new Honda(honLoc));
        locate.add(new Toyota("8, 9"));
        locate.add(new GMC(3, 8));
-
-
-       double[] home = {0, 0};
-
-
-       String printout = "\n\n" +
+	   
+	   String printout = "\n\n" +
                "==========================" + "\nStarting locations...";
 
 
@@ -21,30 +20,45 @@ public class Satellite
        {
            printout += "\nLocation for " + l.getID() + ": (" + getLocation(l.getLoc()) + ")";
        }
+	   
+       printout += "\n\nDistance from home...";
 
+		for(Location l : locate)
+		{
+			printout += "\nDistance for " + l.getID() + ": (" + getDistance(l.getLoc(), home) + ")";
+		}
+		
+		printout += "\n\n==========================\n";
+		
+		for (Location l : locate)
+		{
+            double one = Math.random() * 99 + 1;
+			double two = Math.random() * 99 + 1;
+			((Car)l).move(one, two);
+			
+			printout += "\n\nAfter " + l.getID() + " moved (" + one + ", " + two + ")"
+            + "\nNew location: " + "(" + one + ", " + two + ")" + "\n";
+		}
 
-       printout += "\n\n" + "==========================" +
-                   "\nDistance from home...";
+		printout += "\n\nDistance from home...";
 
+		for (Location l : locate)
+		{
+			printout += "\nDistance for " + l.getID() + ": " + getDistance(l.getLoc(), home);
+		}
+		
+		printout += "\n\n==========================\n";
 
-       for (Location l : locate)
-       {
-           printout += "\nDistance for " + l.getID() + ": (" + getDistance(l.getLoc(), home)+ ")";
-       }
+		System.out.println(printout);
+	}
 
+	public static String getDistance(double[] car, double[] home) 
+	{
+		return String.format("(%.2f)", Math.sqrt((Math.pow(car[0] - home[0], 2)+ Math.pow(car[1] - home[1], 2))));
+	}
 
-       System.out.println(printout);
-   }
-
-
-   public static double getDistance(double[] car, double[] home)
-   {
-       return Math.sqrt((Math.pow(car[0] - home[0], 2)+ Math.pow(car[1] - home[1], 2)));
-   }
-
-
-   public static String getLocation(double[] loc)
-   {
-       return loc[0] + ", " + loc[1];
-   }
+	public static String getLocation(double[] loc) 
+	{
+		return String.format("(%.2f, %.2f)", loc[0], loc[1]);
+	}	
 }
