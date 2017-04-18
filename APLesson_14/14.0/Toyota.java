78dc561f3c656c007fd635c880080fe873c62528
nergisp <poseindonpiril@gmail.com>
2017-04-18 08:33:15 -0700

    Update Toyota.java

--- APLesson_14/14.0/Toyota.java
+++ APLesson_14/14.0/Toyota.java
@@ -1,32 +1,42 @@
-import java.util.Random;
-import java.util.ArrayList;
-
-public class Toyota implements Location {
-	private int[] location;
-	private int x,y;
+public class Toyota implements Location
+{
+	private double[] location;
+	private double xcoor;
+	private double ycoor;
 	
-	public Toyota() {
-		super();
-		l = new double[2];
+	public Toyota()
+	{
+		location = new double[2];
 	}
 	
-	public Toyota() {
-		x = Integer.parseInteger(l.substring(0,1));
-		y = Integer.parseInteger(l.substring(3));
+	public Toyota(String l)
+	{
+		//String[] locs = l.split(", ");
+		//location[0] = Double.parseDouble(locs[0]);
+		//location[1] = Double.parseDouble(locs[1]);
+		
+		xcoor = Double.parseDouble(l.substring(0,1));
+		ycoor = Double.parseDouble(l.substring(3));
 	}
 	
-	public int getID() {
-		Random rand = new Random();
-		return rand.nextInt(900000) + 100000;
+	public int getID()
+	{
+		return (int) (Math.random() * 1000000) + 1;
 	}
 	
-	public void move(int xx, int yy) {
-		x = xx;
-		y = yy;
+	public void move(double x, double y)
+	{
+		//location[0] += x;
+		//location[1] += y;
+		
+		xcoor = x;
+		ycoor = y;
 	}
 	
-	public int[] getLoc() {
-		int[] coordinates = {x,y};
-		return coordinates;
+	public double[] getLoc()
+	{
+		double [] location = {xcoor, ycoor};  
+		return location;
 	}
+	
 }
