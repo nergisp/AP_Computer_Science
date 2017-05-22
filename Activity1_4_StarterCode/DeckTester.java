dd70e396440e1c5934fe5ed603b68b8dc67181d6
nergisp <poseindonpiril@gmail.com>
2017-05-22 16:53:54 -0700

    Update DeckTester.java

--- Activity1_4_StarterCode/DeckTester.java
+++ Activity1_4_StarterCode/DeckTester.java
@@ -1,16 +1,11 @@
-/**
- * This is a class that tests the Deck class.
- */
-public class DeckTester {
 
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		String[] rank1 = {"Jack", "Queen", "King"};
-		String[] suit1 = {"Hearts", "Spades", "Diamonds"};
-		int[] value1 = {11, 12, 13};
+public class DeckTester
+{
+	public static void main(String[] args)
+	{
+	    String[] rank1 = {"Jack", "Queen", "King"};
+	    String[] suit1 = {"Hearts", "Spades", "Diamonds"};
+	    int[] value1 = {11, 12, 13};
 	    Deck deck1 = new Deck(rank1, suit1, value1);
 	    System.out.println("Deck1 size should be 3: " + deck1.size());
 	    
