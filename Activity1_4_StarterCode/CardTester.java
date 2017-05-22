54cf4ef3ae2cf7179a74fe4cd819d19610245a3c
nergisp <poseindonpiril@gmail.com>
2017-05-22 16:56:56 -0700

    Update CardTester.java

--- Activity1_4_StarterCode/CardTester.java
+++ Activity1_4_StarterCode/CardTester.java
@@ -1,13 +1,8 @@
-/**
- * This is a class that tests the Card class.
- */
-public class CardTester {
+public class CardTester
+{
 
-	/**
-	 * The main method in this class checks the Card operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		Card card1 = new Card("Heart", "9", 9);
 		Card card2 = new Card("Heart", "9", 9);
 		Card card3 = new Card("Heart", "Jack", 11);
